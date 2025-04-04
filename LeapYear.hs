1a62cb155ef9e72707fdbb7556211c96ba36c7c6
N. Connor Whitaker <Connornwhit@gmail.com>
2025-04-04 16:34:34 -0400

    Update LeapYear.hs

--- LeapYear.hs
+++ LeapYear.hs
@@ -12,25 +12,25 @@ isLeapYear x
   | otherwise = False -- If none are True then its not a leap year confirmed. 
 -- =================================================================================================================================================
 
-Explanation : I used guards here because of the mutiple cases. I use the mod function here to calculate the year. There are many ways to use mod in Haskell.
-here are 2 ways you can use it.
+-- Explanation : I used guards here because of the mutiple cases. I use the mod function here to calculate the year. There are many ways to use mod in Haskell.
+-- here are 2 ways you can use it.
 
-mod x y -- You use it as a function with 2 arguments, the number "x" you want to mod and "y" the number you want to mod by.
-x `mod` y -- You can also use it as a infix notation, it does the same thing. Imagine it like when you do "x + y" or "x - y"
+-- mod x y -- You use it as a function with 2 arguments, the number "x" you want to mod and "y" the number you want to mod by.
+-- x `mod` y -- You can also use it as a infix notation, it does the same thing. Imagine it like when you do "x + y" or "x - y"
 
 
-Here are some visual examples!!!!
+-- Here are some visual examples!!!!
 
-EX1: 2000 % 400 = 0 (Confirmed Leap year!!!)
+-- EX1: 2000 % 400 = 0 (Confirmed Leap year!!!)
  
-EX2: 1987 % 400 = 387...
-     1987 % 100 = 87...
-     1987 % 4 = 3... (Confimred NOT a Leap year)
+-- EX2: 1987 % 400 = 387...
+--      1987 % 100 = 87...
+ --     1987 % 4 = 3... (Confimred NOT a Leap year)
 
-EX3: 2025 % 400 = 25...
-     2025 % 100 = 25...
-     2025 % 4 = 1... (Confirmed NOT a Leap year)
+-- EX3: 2025 % 400 = 25...
+ --     2025 % 100 = 25...
+--      2025 % 4 = 1... (Confirmed NOT a Leap year)
 
-EX4: 1900 % 400 = 300..
-     1900 % 100 = 0 ... (Oh maybe its not a leap year?)
-     1900 % 4 = 0 (Oh wait it is a Leap year!!! Gotta be carful with when you do your cases to avoid checking the wrong thing at the wrong time!)
+-- EX4: 1900 % 400 = 300..
+--      1900 % 100 = 0 ... (Oh maybe its not a leap year?)
+--      1900 % 4 = 0 (Oh wait it is a Leap year!!! Gotta be carful with when you do your cases to avoid checking the wrong thing at the wrong time!)
